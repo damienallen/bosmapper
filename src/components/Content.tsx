388a8e273f0eac336d83d6f65ed9b10436c58e70
Damien Allen <contact@dallen.co>
2020-05-24 02:01:27 +0200

    Loading screen

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
 import { observer, MobXProviderContext } from 'mobx-react'
-import { IonContent } from '@ionic/react'
+import { IonContent, IonSpinner, IonToast } from '@ionic/react'
 
 import { MapCanvas } from './MapCanvas'
 import { MapOverlay } from './MapOverlay'
@@ -14,16 +15,48 @@ const useStores = () => {
     return React.useContext(MobXProviderContext)
 }
 
-export const Content: React.FC = observer(() => {
-    const { species } = useStores()
-    return (
-        <IonContent id="main">
-            <SettingsModal />
-            {species.count > 0 ? <SpeciesSelector /> : null}
-            <LoginPopover />
+const useStyles = createUseStyles({
+    loading: {
+        background: '#333',
+        height: '100%',
+        width: '100%',
+        color: '#fff',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+    },
+    loadingText: {
+        paddingBottom: 5
+    }
+})
 
-            <MapOverlay />
-            <MapCanvas />
-        </IonContent>
-    )
+export const Content: React.FC = observer(() => {
+    const classes = useStyles()
+    const { species, ui } = useStores()
+
+    return species.count > 0 ?
+        (
+            <IonContent id='main'>
+                <SettingsModal />
+                <SpeciesSelector />
+                <LoginPopover />
+
+                <MapOverlay />
+                <MapCanvas />
+            </IonContent>
+        ) : (
+            <IonContent id='main'>
+                <div className={classes.loading}>
+                    <div className={classes.loadingText}>Aan het laden</div>
+                    <IonSpinner name='dots' color='light' />
+                </div>
+                <IonToast
+                    isOpen={ui.showToast}
+                    onDidDismiss={() => ui.hideToast()}
+                    message={ui.toastText}
+                    duration={2000}
+                />
+            </IonContent>
+        )
 })
