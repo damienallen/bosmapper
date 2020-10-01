bc40e3e2534efa27817673977f895a7d331b4388
Damien Allen <contact@dallen.co>
2020-10-01 22:53:47 +0200

    More logo styling

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -12,7 +12,7 @@ const useStyles = createUseStyles({
         width: '100%',
         padding: '22px 0',
         background: (props: StyleProps) => props.background,
-        color: '#fff',
+        color: (props: StyleProps) => props.color,
         textAlign: 'center',
         fontSize: '1.4em',
         opacity: 0.8
@@ -20,14 +20,17 @@ const useStyles = createUseStyles({
 })
 
 interface StyleProps {
+    color: 'string',
     background: 'string',
 }
 
 export const Logo: React.FC = observer(() => {
     const { settings } = useStores()
 
+    const textColor = settings.authenticated ? '#000' : '#fff'
     const backgroundColor = settings.authenticated ? '#FFA63A' : '#78a658'
     const styleProps = {
+        color: textColor,
         background: backgroundColor,
     }
     const classes = useStyles(styleProps)
